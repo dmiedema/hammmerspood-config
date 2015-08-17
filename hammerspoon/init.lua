09ec538eb60b01865f8a873e2469994b42972d42
dmiedema <danielmiedema+github@me.com>
2015-08-16 20:59:24 -0700

    Add window movements

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -22,6 +22,42 @@ hs.hotkey.bind({"alt"}, "Z", function()
   focusedApplication:selectMenuItem(zoomMenuItem)
 end)
 
+-- Movement
+
+hs.hotkey.bind({'cmd', 'alt', 'ctrl'}, 'H', function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+
+  f.x = f.x - 10
+  
+  win:setFrame(f)
+end)
+
+hs.hotkey.bind({'cmd', 'alt', 'ctrl'}, 'J', function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+
+  f.y = f.y + 10
+  
+  win:setFrame(f)
+end)
+
+hs.hotkey.bind({'cmd', 'alt', 'ctrl'}, 'K', function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+
+  f.y = f.y - 10
+  
+  win:setFrame(f)
+end)
+hs.hotkey.bind({'cmd', 'alt', 'ctrl'}, 'L', function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+
+  f.x = f.x + 10
+  
+  win:setFrame(f)
+end)
 -- Wifi events
 local wifiWatcher = nil
 local homeSSID = "miedema"
